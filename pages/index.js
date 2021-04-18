1c4f00608a39c274b42b0010cc1fd27c4cb8ee7d
hanifrev <hanifrev@gmail.com>
2021-04-18 23:12:54 +0700

    add

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,9 @@ export default function Fair1() {
             rel="stylesheet"
           />
         </Head>
-        <header className="flex flex-row absolute justify-between top-0 w-full px-3 pt-3 sm:top-0 md:top-6">
+        <header className="flex flex-row absolute justify-between top-0 w-full sm:w-11/12 px-3 pt-3 sm:top-0 md:top-6">
           <h1 className="text-base font-bold text-white sm:text-2xl">
-            Product Name
+            Deadly Sun Ent.
           </h1>
           <h1 className="text-base font-bold text-white sm:text-xl">
             Language
@@ -34,18 +34,19 @@ export default function Fair1() {
             <div className="w-full pb-16 sm:pb-0">
               <div>
                 <h5 className="text-white text-sm pb-2 sm:text-2xl leading-7 sm:pb-8">
-                  Above The Sky
+                  Sumerian Records Present
                 </h5>
               </div>
               <div>
-                <h1 className="text-white text-3xl font-bold pb-2 tracking-wide sm:text-7xl leading-10 sm:pb-8 ">
-                  Between The Buried and Me 2020 Tour
+                <h1 className="text-white text-3xl font-bold pb-2 tracking-wide sm:text-6xl leading-10 sm:pb-8 ">
+                  Between The Buried and Me | Spring Tour 2018
                 </h1>
               </div>
               <div>
-                <p className="text-white text-sm sm:text-2xl font-light pb-10">
+                <p className="text-white text-sm sm:text-xl font-light pb-10">
                   Grammy nominated progressive death metal band will coming to
-                  your City. Sign up to get special offers
+                  your city with special guest The Dear Hunter and Leprous. Sign
+                  up to get special offers
                 </p>
               </div>
               <form className="flex flex-col sm:flex-row">
@@ -56,17 +57,19 @@ export default function Fair1() {
                   placeholder="Email"
                 />
                 <Button
-                  className="h-11 bg-white w-full rounded-md text-skyblue text-xs font-bold sm:w-28"
+                  className="h-11 bg-white w-full rounded-md text-skyblue text-xs font-bold sm:w-28 hover:bg-indigo-200"
                   value="Sign Up Now"
                 />
               </form>
             </div>
             {/* RIGHT SIDE */}
             <div className="w-full">
-              <div className="flex w-full mx-auto">
+              <div className="flex w-full mx-auto my-auto">
                 <img
-                  className="relative pl-0 sm:pl-20 md:fixed md:pl-0 lg:pl-20"
-                  src="https://res.cloudinary.com/avl/image/upload/v1611051545/single-page-landing/Group_9.png"
+                  className="relative mx-auto md:mx-32 w-full md:w-4/12 lg:w-4/12 pl-0 sm:pl-0 md:fixed md:pl-0 sm:pt-10 md:pt-0 md:my-6 lg:my-0 "
+                  // src="https://res.cloudinary.com/avl/image/upload/v1611051545/single-page-landing/Group_9.png"
+                  src="https://i.imgur.com/a7fbf2C.jpg"
+                  // src="https://i.imgur.com/e90Y6Yj.jpg"
                   alt="test"
                 />
               </div>
